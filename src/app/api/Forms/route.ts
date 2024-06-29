a7a85f5df2dd096a5c79137227253c3fff577458
prkpro <prakashpandey594409@gmail.com>
2024-06-29 20:24:21 +0530

    Removed comments

--- src/app/api/Forms/route.ts
+++ src/app/api/Forms/route.ts
@@ -1,19 +1,10 @@
 "use server";
 import { getXataClient } from "@/xata";
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
 const xata = getXataClient();
-// type ResponseData = {
-//   message:string,
-//   status:symbol
-// }
 
 export async function POST(request: Request) {
-  // People on the internet can be mean, so let's make sure we don't allow
-  // anyone to create images in the live demo
-  // if (process.env.READ_ONLY === 'true') {
-  //   return NextResponse.json({ message: 'Read only mode enabled' }, { status: 403 });
-  // }
   if (request.method === "POST") {
     const formData = await request.json();
     // Get the form data
@@ -25,7 +16,6 @@ export async function POST(request: Request) {
       return NextResponse.json({message: "Empty Request!"}, {status:204})
     }
     try {  
-    // Create an empty image record with no base64 content
     const record = await xata.db.Forms.create({email:email, name:name, description:message});  
     return NextResponse.json({message: "Ok!"}, {status:200});
     }
@@ -37,7 +27,4 @@ export async function POST(request: Request) {
   else {
     return NextResponse.json({message: "Bad request!"}, {status:405});
   }
-  
-
-  
 }
