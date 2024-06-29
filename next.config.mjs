38d18d97d203cd990b1abcf0ba491c7512ef1fb1
prkpro <prakashpandey594409@gmail.com>
2024-06-29 13:27:47 +0530

    updated base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/formly",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
